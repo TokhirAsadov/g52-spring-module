8331cb04b35443fb0e4d8820830a04af9a0ff9f1
Takhir <guvalakat1603@gmail.com>
2025-07-09 18:44:14 +0500

    Add properties for colors and courses in MyBean2 and update XML configuration

--- lesson_1/src/main/java/uz/pdp/Main.java
+++ lesson_1/src/main/java/uz/pdp/Main.java
@@ -14,6 +14,7 @@ public class Main {
 
         MyBean2 bean2 = context.getBean(MyBean2.class);
         bean2.saySomething2();
+        System.out.println(bean2);
 
     }
 }
--- lesson_1/src/main/java/uz/pdp/MyBean2.java
+++ lesson_1/src/main/java/uz/pdp/MyBean2.java
@@ -1,11 +1,33 @@
 package uz.pdp;
 
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+import java.util.Map;
+
+@ToString
 public class MyBean2 {
+
+    @Getter
+    private List<String> colors;
+    // Set<String>
+
+    @Getter
+    private Map<Integer,String> courses;
 
     private MyBean myBean;
 
     public MyBean2(MyBean myBean5){
         this.myBean = myBean5;
+    }
+
+    public void setColors(List<String> colors) {
+        this.colors = colors;
+    }
+
+    public void setCourses(Map<Integer, String> courses) {
+        this.courses = courses;
     }
 
     public void saySomething2(){
