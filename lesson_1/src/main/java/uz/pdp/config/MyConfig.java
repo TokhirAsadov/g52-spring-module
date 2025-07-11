7f708676906c48f8b8b69092d3d4d7bd30519111
Takhir <guvalakat1603@gmail.com>
2025-07-11 17:09:17 +0500

    Add application properties for PostgreSQL configuration and update MyConfig to load properties

--- lesson_1/src/main/java/uz/pdp/config/MyConfig.java
+++ lesson_1/src/main/java/uz/pdp/config/MyConfig.java
@@ -1,7 +1,9 @@
 package uz.pdp.config;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @ComponentScan("uz.pdp")
+@PropertySource("classpath:application.properties")
 public class MyConfig {
 }
