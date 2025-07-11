974ad0c71de88b490065fd1c85410d305129e557
Takhir <guvalakat1603@gmail.com>
2025-07-11 17:19:12 +0500

    Add Product class with fields for name and price

---
+++ lesson_1/src/main/java/uz/pdp/Product.java
@@ -0,0 +1,27 @@
+package uz.pdp;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class Product {
+    private String name;
+    private Double price;
+
+    public Product() {
+    }
+
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
