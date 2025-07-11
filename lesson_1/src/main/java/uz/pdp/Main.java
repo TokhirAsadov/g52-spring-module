2fb326ef060816298480606f01e9640785eeedbe
Takhir <guvalakat1603@gmail.com>
2025-07-11 17:06:18 +0500

    Add PropertySource annotation to MainApplicationConfig for application properties

--- lesson_1/src/main/java/uz/pdp/Main.java
+++ lesson_1/src/main/java/uz/pdp/Main.java
@@ -1,11 +1,29 @@
 package uz.pdp;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 
 
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MainApplicationConfig.class);
+
+        Environment environment = (Environment) context.getEnvironment();
+        String url = environment.getProperty("spring.datasource.url");
+        String database = environment.getProperty("spring.datasource.database");
+        String user = environment.getProperty("spring.datasource.user");
+        String password = environment.getProperty("spring.datasource.password");
+        String schema = environment.getProperty("spring.datasource.schema");
+        String driver = environment.getProperty("spring.datasource.driver");
+        System.out.println("****************************************");
+        System.out.println(url);
+        System.out.println(database);
+        System.out.println(user);
+        System.out.println(password);
+        System.out.println(schema);
+        System.out.println(driver);
+        System.out.println("****************************************");
 
         MyBean bean = context.getBean(MyBean.class);
         bean.saySomething();
