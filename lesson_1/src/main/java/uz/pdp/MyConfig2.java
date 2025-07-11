d0cc95e516a250ae355909fa9edd131a39a39f0c
Takhir <guvalakat1603@gmail.com>
2025-07-11 16:46:51 +0500

    Add MyConfig2 class for Spring configuration and MyBean2 bean definition

---
+++ lesson_1/src/main/java/uz/pdp/MyConfig2.java
@@ -0,0 +1,14 @@
+package uz.pdp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfig2 {
+
+    @Bean
+    public MyBean2 myBean2(MyBean myBean){
+        return new MyBean2(myBean);
+    }
+
+}
