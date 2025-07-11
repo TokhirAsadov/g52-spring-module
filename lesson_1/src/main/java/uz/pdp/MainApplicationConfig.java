b1e0cd3c47bece8d1761375d744847cf47842928
Takhir <guvalakat1603@gmail.com>
2025-07-11 17:06:14 +0500

    Add PropertySource annotation to MainApplicationConfig for application properties

--- lesson_1/src/main/java/uz/pdp/MainApplicationConfig.java
+++ lesson_1/src/main/java/uz/pdp/MainApplicationConfig.java
@@ -2,8 +2,10 @@ package uz.pdp;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @Import({MyConfig.class,MyConfig2.class})
+@PropertySource("classpath:application.properties")
 public class MainApplicationConfig {
 }
