95cc420cd8638e608699d9ca0d6633a64062d42d
Takhir <guvalakat1603@gmail.com>
2025-07-11 16:46:43 +0500

    Update main application configuration class reference in Main.java

--- lesson_1/src/main/java/uz/pdp/Main.java
+++ lesson_1/src/main/java/uz/pdp/Main.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MainApplicationConfig.class);
 
         MyBean bean = context.getBean(MyBean.class);
         bean.saySomething();
