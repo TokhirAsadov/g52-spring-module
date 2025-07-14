f89154bf283b124b348337489e228351047500ae
Takhir <guvalakat1603@gmail.com>
2025-07-14 17:47:03 +0500

    java config scope

--- lesson_1/src/main/java/uz/pdp/Main.java
+++ lesson_1/src/main/java/uz/pdp/Main.java
@@ -7,15 +7,24 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
 
-        //AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ioc-settings.xml");
-        User user1 = context.getBean(User.class);
-        System.out.println("before change user1: "+user1);
-        User user2 = context.getBean(User.class);
-        System.out.println("user2: "+user2);
-        System.out.println("======================================");
-        user1.setFullName("Anvar Ganiyev");
-        System.out.println("after change user1: "+user1);
-        System.out.println("user2: "+user2);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ioc-settings.xml");
+//        User user1 = context.getBean(User.class);
+//        System.out.println("before change user1: "+user1);
+//        User user2 = context.getBean(User.class);
+//        System.out.println("user2: "+user2);
+//        System.out.println("======================================");
+//        user1.setFullName("Anvar Ganiyev");
+//        System.out.println("after change user1: "+user1);
+//        System.out.println("user2: "+user2);
+
+        User2 user21 = context.getBean(User2.class);
+        User2 user22 = context.getBean(User2.class);
+        System.out.println("user2 1: "+user21);
+        System.out.println("user2 2: "+user22);
+        user21.setAge(88);
+        System.out.println("============================");
+        System.out.println("user2 1: "+user21);
+        System.out.println("user2 2: "+user22);
     }
 }
--- lesson_1/src/main/java/uz/pdp/MyConfig.java
+++ lesson_1/src/main/java/uz/pdp/MyConfig.java
@@ -1,7 +1,16 @@
 package uz.pdp;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class MyConfig {
+
+    @Bean
+    @Scope("prototype")
+    public User2 user2(){
+        return new User2("Ali Valiyev 2",14);
+    }
+
 }
