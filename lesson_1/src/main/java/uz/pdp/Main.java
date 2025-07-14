73b7d2dccfe26b0852c635716f09ad8bc5ca01b2
Takhir <guvalakat1603@gmail.com>
2025-07-14 17:15:00 +0500

    bean_scope

--- lesson_1/src/main/java/uz/pdp/Main.java
+++ lesson_1/src/main/java/uz/pdp/Main.java
@@ -1,26 +1,12 @@
 package uz.pdp;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import uz.pdp.config.AutoConfig;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AutoConfig.class);
-//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
-//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ioc-settings.xml");
-//
-        Car car = context.getBean(Car.class);
-        System.out.println(car);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
 
-        User user = context.getBean(User.class);
-        System.out.println(user);
-
-        /*
-        * Car(name, color, price)
-        * init, destroy
-        * xml, java, auto config
-        * */
     }
 }
--- lesson_1/src/main/java/uz/pdp/config/AutoConfig.java
+++
@@ -1,7 +0,0 @@
-package uz.pdp.config;
-
-import org.springframework.context.annotation.ComponentScan;
-
-@ComponentScan("uz.pdp")
-public class AutoConfig {
-}
