df1596ee4f270b4bc20713099520f3822c561669
Takhir <guvalakat1603@gmail.com>
2025-07-14 18:07:17 +0500

    xml config lazy

--- lesson_1/src/main/java/uz/pdp/Main.java
+++ lesson_1/src/main/java/uz/pdp/Main.java
@@ -1,14 +1,15 @@
 package uz.pdp;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyAutoConfig.class);
+
         // todo -- .xml - config
-//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ioc-settings.xml");
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ioc-settings.xml");
 //        User user1 = context.getBean(User.class);
 //        System.out.println("before change user1: "+user1);
 //        User user2 = context.getBean(User.class);
@@ -29,13 +30,15 @@ public class Main {
 //        System.out.println("user2 1: "+user31);
 //        System.out.println("user2 2: "+user32);
 
-        User3 user31 = context.getBean(User3.class);
-        User3 user32 = context.getBean(User3.class);
-        System.out.println("user3 1: "+ user31);
-        System.out.println("user3 2: "+ user32);
-        user31.setAge(88);
-        System.out.println("============================");
-        System.out.println("user3 1: "+ user31);
-        System.out.println("user3 2: "+ user32);
+        // todo ---- auto - config
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyAutoConfig.class);
+//        User3 user31 = context.getBean(User3.class);
+//        User3 user32 = context.getBean(User3.class);
+//        System.out.println("user3 1: "+ user31);
+//        System.out.println("user3 2: "+ user32);
+//        user31.setAge(88);
+//        System.out.println("============================");
+//        System.out.println("user3 1: "+ user31);
+//        System.out.println("user3 2: "+ user32);
     }
 }
--- lesson_1/src/main/java/uz/pdp/User.java
+++ lesson_1/src/main/java/uz/pdp/User.java
@@ -24,4 +24,12 @@ public class User {
     public void setAge(Integer age) {
         this.age = age;
     }
+
+    public void init(){
+        System.out.println(getClass().getName()+" ning init() methodi ishladi...............");
+    }
+
+    public void destroy(){
+        System.out.println(getClass().getName()+" ning destroy() methodi ishladi...............");
+    }
 }
